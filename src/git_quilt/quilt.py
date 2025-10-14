c1dc1a4942e5ca1b178feba89584bdcf0d2d0bf8
Lawrence D'Anna <larry@elder-gods.org>
2025-10-14 08:58:24 -0400

    added QuiltFile class to represent .git-quilt files

---
+++ src/git_quilt/quilt.py
@@ -0,0 +1,76 @@
+from dataclasses import dataclass, field, fields
+from typing import List, Self, IO
+from io import StringIO
+
+import yaml
+
+
+class YAMLObject(yaml.YAMLObject):
+
+    # Override to_yaml to customize the yaml representation.
+    #   * Order of fields is as declared in the dataclass.
+    #   * False values are skipped.
+    #   * Multiline strings are represented with pipe-style yaml strings.
+    @classmethod
+    def to_yaml(cls, dumper: yaml.Dumper, data: Self):
+        def i():
+            for f in fields(cls):  # type: ignore
+                value = getattr(data, f.name)
+                if not value:
+                    continue
+                if isinstance(value, str) and "\n" in value:
+                    rep = dumper.represent_scalar("tag:yaml.org,2002:str", value, style="|")
+                else:
+                    rep = dumper.represent_data(value)
+                yield (dumper.represent_data(f.name), rep)
+
+        return yaml.MappingNode(cls.yaml_tag, list(i()))
+
+
+class Loader(yaml.SafeLoader):
+
+    # By default, PyYAML uses __new__() and .__dict__.update() to construct
+    # objects.  Use the constructor provided by dataclasses instead, so that
+    # defaults are respected and unknown fields raise exceptions.
+    def construct_yaml_object(self, node, cls):
+        state = self.construct_mapping(node, deep=True)
+        return cls(**state)  # type: ignore
+
+
+@dataclass
+class Baseline(YAMLObject):
+    yaml_tag = "!Baseline"
+    yaml_loader = Loader
+    sha: str
+    ref: str | None = field(default=None)
+    remote: str | None = field(default=None)
+
+
+yaml.add_path_resolver("!QuiltFile", [], Loader=Loader)
+yaml.add_path_resolver("!Baseline", ["baselines", None], Loader=Loader)
+
+
+@dataclass
+class QuiltFile(YAMLObject):
+    yaml_tag = "!QuiltFile"
+    yaml_loader = Loader
+    title: str | None = field(default=None)
+    description: str | None = field(default=None)
+    baselines: List[Baseline] = field(default_factory=list)
+
+    def dump(self, f: IO):
+        yaml.dump(self, f)
+
+    def dumps(self) -> str:
+        with StringIO() as f:
+            yaml.dump(self, f)
+            return f.getvalue()
+
+    @classmethod
+    def load(cls, f: IO) -> Self:
+        return yaml.load(f, Loader=Loader)
+
+    @classmethod
+    def loads(cls, s: str) -> Self:
+        with StringIO(s) as f:
+            return yaml.load(f, Loader=Loader)
---
+++ test/test_quilt.py
@@ -0,0 +1,40 @@
+from textwrap import dedent
+from git_quilt.quilt import QuiltFile
+
+
+def test_yaml():
+
+    q = QuiltFile.loads("{}")
+    assert q.description is None
+    assert q.baselines == []
+
+    y = """
+        title: my branch
+        description: |
+          This is a branch.
+          Foo Bar Baz
+        baselines:
+        - sha: xyz
+        - sha: abcdef
+          ref: bar
+          remote: https://example.com/project.git
+    """
+
+    q = QuiltFile.loads(dedent(y))
+    assert q.description == "This is a branch.\nFoo Bar Baz\n"
+    foo, bar = q.baselines
+    assert foo.ref is None
+    assert foo.remote is None
+    assert foo.sha == "xyz"
+    assert bar.ref == "bar"
+    assert bar.remote == "https://example.com/project.git"
+    assert bar.sha == "abcdef"
+
+    assert dedent(y).strip() == q.dumps().strip()
+
+    try:
+        QuiltFile.loads("lol: wtf")
+    except Exception:
+        pass
+    else:
+        raise Exception("parse should have failed")
